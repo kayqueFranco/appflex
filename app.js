69ef4e82d1cbb65881c2f8a58c6f24b835fccd36
kayqueFranco <kayque.franco2018@gmail.com>
2025-01-22 15:19:17 -0300

    app flex

---
+++ app.js
@@ -0,0 +1,22 @@
+/**
+ * APP - Flex
+ * @Author Kayque Franco
+ */
+
+// declaração de variaveis 
+let etanol, gasolina 
+
+function calcular() {
+    //entrada de dados 
+    etanol= frmFlex.inputEtanol.value
+    gasolina= frmFlex.inputGasolina.value
+    //console.log(etanol)
+    //console.log(gasolina)
+    if(etanol <=0.7 * gasolina) {
+        document.getElementById('status').src = "img/etanol.png"
+    }
+    else {
+        document.getElementById('status').serc = "img/gasolina.png"
+    }
+    return false
+}
